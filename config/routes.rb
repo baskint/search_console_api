fbb661af49cfdce76ce21c4c4d39677ae89b0740
Baskin Tapkan <baskint@gmail.com>
2023-05-16 14:58:49 -0500

    clean slate

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get 'google_search_console/authorize'
-  get 'google_search_console/callback'
-end
